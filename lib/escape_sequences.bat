24a3af5086475cbfd5417e0226c23322772abc5c
Relique <lorenzomatrullo04@gmail.com>
2025-01-06 20:35:16 +0100

    escape sequences

---
+++ lib/escape_sequences.bat
@@ -0,0 +1,15 @@
+:: This is a comment
+rem This is comment too
+
+
+:: Sets the window name of the program
+title 1st program
+
+:: Clears the screen.
+cls
+
+:: Pauses the execution of the batch file and waits for the user to press any key to continue
+pause
+
+:: Ends the batch file or closes the Command Prompt window.
+exit
