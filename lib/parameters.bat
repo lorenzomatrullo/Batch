b6d7cec677d1ecd80f5437091ac39a1396f69a54
Relique <lorenzomatrullo04@gmail.com>
2025-01-08 11:21:47 +0100

    adding parameters

---
+++ lib/parameters.bat
@@ -0,0 +1,11 @@
+:: /f - Forces deletion of read-only files.
+del /f file.txt
+
+:: /q - Quiet mode: Suppresses confirmation prompts.
+xcopy source destination /q
+
+:: /y - Automatically answers yes to overwrite prompts.
+xcopy source destination /y
+
+:: /v - Verifies the files after they are copied.
+xcopy source destination /v
